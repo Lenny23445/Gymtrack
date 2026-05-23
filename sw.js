eabbfeef984a12bfa6a77d8dc3cc776deafd70ad
Lenny23445 <lenny23445@gymtrack.local>
2026-05-23 17:10:07 +0200

    Fix: Encoding wiederhergestellt + SW-Update-Fix + Begleiter-Toggle (sauber UTF-8)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-﻿/* GymTrack â€” Service Worker */
-const CACHE = 'gymtrack-v202605231702';
+/* GymTrack â€” Service Worker */
+const CACHE = 'gymtrack-v202605231709';
 const SHELL = [
   './index.html',
   './manifest.json',
